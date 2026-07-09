5bf8a847a4cf96e4871a822d4860506ac37d0580
Manoj G K <manojgk0210@gmail.com>
2026-07-09 13:57:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('ok');
+    return;
+  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World from Ansible-deployed app!');
+});
+
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
